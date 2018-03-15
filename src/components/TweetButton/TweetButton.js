bbed13bde989bf1f79de72f6b8d2f6e23b7b6e80
DungDinhDungDinh <channhoibong1995@gmail.com>
2018-03-15 21:27:41 +0700

    Init source my-tweeter

---
+++ src/components/TweetButton/TweetButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+class TweetButton extends React.PureComponent {
+  render() {
+    return (
+      <Button bsStyle="primary">Primary</Button>
+    );
+  }
+}
+
+export default TweetButton;
